4d5f319935354d0d0831c9ec090b5ad08c87749e
omgede <64190947+omgede@users.noreply.github.com>
2020-05-01 17:15:22 +0700

    Membuat folder HackerRank dan memindahkan beberapa file kesitu

---
+++ handzdirty/Basics/sortOnce.py
@@ -0,0 +1,5 @@
+a_set = {1,2,3,4,4,5}
+
+print(a_set)
+a_list = list(a_set)
+print(a_list)
---
+++ handzdirty/HackerRank/nilaiRata2.py
@@ -0,0 +1,19 @@
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+
+# 56.00
+
+
+
+#Input jumlah anak
+n = input("Input jumlah anak: ")
+x = int(n)-1
+anak = [][]
+
+#Input nama anak dan nilainya
+for i in range(n):
+    
+
